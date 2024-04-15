5851003a632a338255058efb781d5952d920bd70
Aishwary Singh <121888871+thakuraishwary@users.noreply.github.com>
2024-04-15 19:20:57 +0530

    Create page.tsx

---
+++ app/(main)/dashboard/page.tsx
@@ -0,0 +1,27 @@
+import { redirect } from 'next/navigation';
+
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile"
+import InitalModal from '@/components/modals/initial-modal';
+
+export default async function SetupPage() {
+  const profile = await initialProfile();
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id
+        }
+      }
+    }
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`)
+  }
+
+  return (
+    <InitalModal />
+  )
+}
